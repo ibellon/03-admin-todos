7e6c5a7d60366d469f617119fa26c7a168dfd658
ENMAD0386\Isidro Bellón <isidro.bellon@entelgy.com>
2026-01-23 15:03:37 +0100

    Api PUT y se termina la sección

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -1,6 +1,6 @@
 import prisma from '@/app/lib/prisma';
-import { Segment } from 'next/dist/shared/lib/app-router-types'
-import { NextResponse, NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import { boolean, object, string } from 'yup'
 
 interface Segments {
   params: Promise<{ id: string }>;
@@ -8,7 +8,41 @@ interface Segments {
 
 export async function GET(request: Request, {params}: Segments) { 
   const id = (await params).id; 
-  
+
+  return NextResponse.json({
+        todo: await getTodo(id, params)
+  })
+}
+
+const putSchema = object({
+  description: string().optional(),
+  complete: boolean().optional()
+})
+
+export async function PUT(request: Request, {params}: Segments) { 
+  try{
+      const id = (await params).id; 
+
+      getTodo(id, params);
+
+      const {complete, description} = 
+        await putSchema.validate(await request.json());
+
+      const updateTodo = await prisma.todo.update({
+        where: {id},
+        data: {complete, description}
+      })
+
+      return NextResponse.json({
+            updateTodo
+      })
+  }
+  catch(error){
+    return NextResponse.json(error, {status: 400})
+  }
+ 
+}
+async function getTodo(id:string, params:any) {
   const todo = await prisma.todo.findFirst({
     where: {id}
   })
@@ -18,8 +52,5 @@ export async function GET(request: Request, {params}: Segments) {
       message: 'No existe el registro con ID: '+id
     }, {status: 404})
   }
-
-  return NextResponse.json({
-        todo
-  })
+  return todo;
 }
