5fbd7edc06dbb8d0bfe8116e6d00f60c6fce688f
ENMAD0386\Isidro Bellón <isidro.bellon@entelgy.com>
2026-01-28 17:31:33 +0100

    Guardando trabajo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+ 
+}
   /* config options here */
-};
+ module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tailus.io',
+      },
+    ],
+  },
+}
 
 export default nextConfig;
