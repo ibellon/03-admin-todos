c0de37eed3398127506727a93915db5602563e6f
ENMAD0386\Isidro Bellón <isidro.bellon@entelgy.com>
2026-02-03 17:30:27 +0100

    Configurando estilos Tailwind

--- app/todos/components/TodoItem.tsx
+++ app/todos/components/TodoItem.tsx
@@ -1,5 +1,6 @@
 import { Todo } from "@prisma/client";
-
+import styles  from "./TodoItem.module.css";
+import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
 interface Props {
     todo: Todo;
@@ -7,8 +8,26 @@ interface Props {
 
 export const TodoItem = ({todo}:Props) => {
     return (
-        <div>
-            {todo.description}
+        <div className={todo.complete ? styles.todoDone: styles.todoPending}>
+            <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
+               
+               <div className= {`
+                    flex p-2 rounded-md cursor-pointer
+                    hover:opacity-60
+                    ${ todo.complete ? 'bg-blue-100': 'bg-red-500'}
+                `}>
+    
+               </div>
+               {
+                todo.complete
+                ? <IoCheckboxOutline size={30}></IoCheckboxOutline>
+                : <IoSquareOutline size={30}></IoSquareOutline>
+               }
+
+               <div className="text-center sm:text-left">
+                    { todo.description }
+               </div>
+            </div>
         </div>
     )
 }
