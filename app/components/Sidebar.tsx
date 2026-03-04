87eea4b9ce205c05329c3af86f91854708282eee
ENMAD0386\Isidro Bellón <isidro.bellon@entelgy.com>
2026-03-04 09:30:49 +0100

    Logout y Login

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { SidebarItem } from './SidebarItem';
 import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCompassOutline, IoListOutline, IoPerson, IoPersonOutline } from 'react-icons/io5';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]/route';
+import { LogoutButton } from './LogoutButton';
 
 const menuItems = [
   {
@@ -88,10 +89,7 @@ export const Sidebar = async () => {
       </div>
 
       <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-          <CiLogout />
-          <span className="group-hover:text-gray-700">Logout</span>
-        </button>
+        <LogoutButton></LogoutButton>
       </div>
     </aside>
   )
